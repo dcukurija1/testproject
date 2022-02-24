c2522353018ebbd65316e52a202446e31194ac32
GreenChicken21 <dcukurija1@etf.unsa.ba>
2022-02-25 00:55:03 +0100

    navbar, start of dummy meetups

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import AllMeetupsPage  from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 function App() {
   return (
     <div>
+      <MainNavigation/>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
     </div>
   );
 }
