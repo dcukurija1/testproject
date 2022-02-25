ad9182dd00f9bf2f72496a475724e23ed52f77f4
GreenChicken21 <dcukurija1@etf.unsa.ba>
2022-02-25 20:19:42 +0100

    form for new meetups and start of a database

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,22 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
 function NewMeetupsPage() {
+    function onAddMeetupHandler(meetupData) {
+        fetch('https://react-testproject-e2b52-default-rtdb.firebaseio.com/meetups.json' ,
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+        );
+    }
     return (
-        <div>New Meetups Page</div>
+        <section>
+            <h1>New Meetup</h1>
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
+        </section>
     );
 }
 
