f63413b8aae9be9ade101fa9235cb8e65f3dd4a6
GreenChicken21 <dcukurija1@etf.unsa.ba>
2022-02-25 02:33:13 +0100

    finished layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import AllMeetupsPage  from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavouritesPage from './pages/Favourites';
 import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 function App() {
   return (
-    <div>
-      <MainNavigation/>
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupsPage />} />
-        <Route path="/favourites" element={<FavouritesPage />} />
-      </Routes>
-    </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupsPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </Layout>
   );
 }
 
